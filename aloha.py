04559653c5be65ed4fa561ecadcd50b0ba366fb3
NourhanElyamany <68388481+NourhanElyamany@users.noreply.github.com>
2023-01-03 23:52:20 +0200

    Update aloha.py

--- aloha.py
+++ aloha.py
@@ -2,8 +2,9 @@ import numpy
 import math
 import random
 
+
 class node:
-    def __init__(self, status =  'inactive', packets = 1):
+    def __init__(self, status =  'inactive', packets = 2):
         self.status = status
         self.packets = packets
 
@@ -17,7 +18,21 @@ def state():
     if random.choice(['active', 'inactive']) == 'active':
         return True
     return False
-    
+
+def transmission(nodes):
+    actives = []
+    for i in range(1,nodes):    #node creation  
+        
+        if state() == True:
+            node().status = 'active'
+            actives.append(node())
+        
+        
+ 
+nodes = 3
+
+print(transmission(nodes))
+   
     
     
 #def poisson_distribution():
