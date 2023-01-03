f9972627af34f20f21452ae40bfacff89645b550
NourhanElyamany <68388481+NourhanElyamany@users.noreply.github.com>
2023-01-03 23:02:12 +0200

    effiency

--- aloha.py
+++ aloha.py
@@ -1,9 +1,16 @@
-
+import numpy
+import math
 
 class node:
     def __init__(self, status =  'inactive', packets = 1):
         self.status = status
         self.packets = packets
 
-def poisson_distribution():
     
+    
+def effiency(nodes):
+    
+    return nodes * math.exp(-2 * nodes)
+    
+def poisson_distribution():
+  
