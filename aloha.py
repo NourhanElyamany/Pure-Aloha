8674d06b8178e36cf8185efb81f0760db393b863
NourhanElyamany <68388481+NourhanElyamany@users.noreply.github.com>
2023-01-03 23:09:10 +0200

    state

--- aloha.py
+++ aloha.py
@@ -1,5 +1,6 @@
 import numpy
 import math
+import random
 
 class node:
     def __init__(self, status =  'inactive', packets = 1):
@@ -11,6 +12,13 @@ class node:
 def effiency(nodes):
     
     return nodes * math.exp(-2 * nodes)
+
+def state():
+    if random.choice(['active', 'inactive']) == 'active':
+        return True
+    return False
     
-def poisson_distribution():
+    
+    
+#def poisson_distribution():
   
