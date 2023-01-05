7abea6bee69b79e1a81463bfe04db5c8546556f2
omar9991 <omar85zaki@gmail.com>
2023-01-06 00:44:03 +0200

    User input number of nodes

--- Pure_Aloha.py
+++ Pure_Aloha.py
@@ -13,6 +13,14 @@ if Tfr == 0.0:
 
 duration = int(input("Please enter duration of simulation(seconds): "))
 
+Node_Number =int(input ("please enter Number of Nodes:  "))
+Node =[]
+
+
+
+
+
+
 transmissions = 0
 failedFrames = 0
 channelBusy = False
@@ -61,7 +69,7 @@ def runALOHAnode():
             failedcurrenttime = time()
             Gaptime = failedcurrenttime- BeginTime
             time_fail.append(Gaptime*1000)
-            
+
             #Put data in queue to tell other sending nodes that this is a collision and their transmitted frame is failed
             q.put(1)
             #Sleeping for Tfr seconds means that node is transmitting and it takes Tfr seconds.
@@ -104,47 +112,52 @@ def runALOHAnode():
                 Gaptime = failedcurrenttime- BeginTime
                 time_fail.append(Gaptime*1000)
             semaphore.release()
+
+for i in range(Node_Number):
+    Node_threads = threading.Thread(target = runALOHAnode)
+    Node_threads.daemon = True
+    Node_threads.start()        
         
 #Creating threads and starting them.
-node1 = threading.Thread(target = runALOHAnode)
-node1.daemon = True
-node1.start()
+# node1 = threading.Thread(target = runALOHAnode)
+# node1.daemon = True
+# node1.start()
 
-node2 = threading.Thread(target = runALOHAnode)
-node2.daemon = True
-node2.start()
+# node2 = threading.Thread(target = runALOHAnode)
+# node2.daemon = True
+# node2.start()
 
-node3 = threading.Thread(target = runALOHAnode)
-node3.daemon = True
-node3.start()
+# node3 = threading.Thread(target = runALOHAnode)
+# node3.daemon = True
+# node3.start()
 
-node4 = threading.Thread(target = runALOHAnode)
-node4.daemon = True
-node4.start()
+# node4 = threading.Thread(target = runALOHAnode)
+# node4.daemon = True
+# node4.start()
 
-node5 = threading.Thread(target = runALOHAnode)
-node5.daemon = True
-node5.start()
+# node5 = threading.Thread(target = runALOHAnode)
+# node5.daemon = True
+# node5.start()
 
-node6 = threading.Thread(target = runALOHAnode)
-node6.daemon = True
-node6.start()
+# node6 = threading.Thread(target = runALOHAnode)
+# node6.daemon = True
+# node6.start()
 
-node7 = threading.Thread(target = runALOHAnode)
-node7.daemon = True
-node7.start()
+# node7 = threading.Thread(target = runALOHAnode)
+# node7.daemon = True
+# node7.start()
 
-node8 = threading.Thread(target = runALOHAnode)
-node8.daemon = True
-node8.start()
+# node8 = threading.Thread(target = runALOHAnode)
+# node8.daemon = True
+# node8.start()
 
-node9 = threading.Thread(target = runALOHAnode)
-node9.daemon = True
-node9.start()
+# node9 = threading.Thread(target = runALOHAnode)
+# node9.daemon = True
+# node9.start()
 
-node10 = threading.Thread(target = runALOHAnode)
-node10.daemon = True
-node10.start()
+# node10 = threading.Thread(target = runALOHAnode)
+# node10.daemon = True
+# node10.start()
 
 #wait for threads to run for a while.
 print("Please wait...\n")
